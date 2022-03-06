058bec03306b92487ed184c5510665a18b86e80b
Scarlett Eller <rq6715@gmail.com>
2022-03-06 16:00:14 -0500

    completed assignment

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,33 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
-  # Add routes
+
+  get '/messages' do
+    messages = Message.all 
+    messages.to_json
+  end
+
+  post '/messages' do
+    added_post = Message.create(
+      body: params[:body],
+      username: params[:username],
+      created_at: params[:created_at],
+      updated_at: params[:updated_at],
+    )
+    added_post.to_json
+  end
+
+  patch '/messages/:id' do 
+    target_post = Message.find(params[:id])
+    target_post.update(
+      body: params[:body]
+    )
+    target_post.to_json
+  end
+
+  delete '/messages/:id' do
+    target_post = Message.find(params[:id])
+    target_post.destroy
+    target_post.to_json
+  end
   
 end
